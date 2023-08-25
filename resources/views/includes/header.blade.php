919320f8e009a498f3756df8e73a1b64e8dbd894
cloudhuh <claudiu.huhulia@gmail.com>
2023-08-25 16:11:39 +0200

    added header & jumbo

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,21 @@
+<header class="container d-flex justify-content-between align-items-center">
+    <figure>
+        <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="logo">
+    </figure>
+    <nav>
+        <ul class="d-flex justify-content-between align-items-center">
+            <li class="fw-bold"><a href="#"> Characters </a></li>
+            <li class="fw-bold"><a href="#"> Comics </a></li>
+            <li class="fw-bold"><a href="#"> Movies </a></li>
+            <li class="fw-bold"><a href="#"> Tv </a></li>
+            <li class="fw-bold"><a href="#"> Games </a></li>
+            <li class="fw-bold"><a href="#"> Collectibles </a></li>
+            <li class="fw-bold"><a href="#"> Videos </a></li>
+            <li class="fw-bold"><a href="#"> Fans </a></li>
+            <li class="fw-bold"><a href="#"> News </a></li>
+            <li class="fw-bold"><a href="#"> Shop </a></li>
+        </ul>
+    </nav>
+</header>
+
+<div class="jumbo"></div>
